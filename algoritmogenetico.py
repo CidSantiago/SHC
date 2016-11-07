db022560dc09a66a438048b8913c8c68f1d44e94
Caio Cid Santiago <caiocid@gmail.com>
2016-11-07 19:21:10 -0300

    Fix: Geracao de populacao inicial   Add: funcao objetivo

--- algoritmogenetico.py
+++ algoritmogenetico.py
@@ -6,10 +6,20 @@ Created on Wed Oct 26 18:20:29 2016
 """
 import numpy as np
 
-#Função objetivo/
+#Função objetivo
 
-def f(t):
-    return np.sin(t)
+def f(cargas, custos, curPop, t):
+    curAptidao = np.zeros(len(curPop))
+    for m in range(0, len(custos)):
+                if custos[m][1]>t and custos[m][2]<t:
+                    custoAtual = custos[m][0]
+                    
+    for i in range(0,len(curPop)):       
+        for j in range(0, len(cargas)):
+            if t >= curPop[i][j] and t< curPop[i][j] + cargas[j][1]:
+                curAptidao[i] = curAptidao[i] + cargas[j][0]*custoAtual 
+                
+    return curAptidao
 
 # Parâmetros do Algoritmo genético
 
@@ -24,4 +34,5 @@ cargas = [[8,3],[7,2],[10,1],[0.5,12]]#[consumo, tempo]
 
 # Criando População inicial
 
-curPop = np.random.choice(np.arange(0,24,1),size=(len(cargas),100))
+curPop = np.random.choice(np.arange(0,24,1),size=(sizePop, len(cargas)))
+
