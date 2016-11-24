0e4807b77d5f9157b2c9ac454ec162c35c86f1d5
Caio Cid Santiago <caiocid@gmail.com>
2016-11-24 15:07:41 -0300

    Add Selection Step & Fix apt. Function
    Added selection Step using Binary Tournament.
    Changed fitness function. Now it receives the entire population and
    returns a vector with all the Fitness

--- algoritmogenetico.py
+++ algoritmogenetico.py
@@ -6,21 +6,54 @@ Created on Wed Oct 26 18:20:29 2016
 """
 import numpy as np
 
-#Função objetivo
+#Funções objetivos
 
-def f(cargas, custos, curPop, t):
+## Custo Minimo
+def f(cargas, custos, curPop):
     curAptidao = np.zeros(len(curPop))
-    for m in range(0, len(custos)):
-                if custos[m][1]>t and custos[m][2]<t:
-                    custoAtual = custos[m][0]
+    for t in range (0,24):
+        
+        for m in range(0, len(custos)):
+            if custos[m][1]>t and custos[m][2]<t:
+                custoAtual = custos[m][0]
                     
-    for i in range(0,len(curPop)):       
-        for j in range(0, len(cargas)):
-            if t >= curPop[i][j] and t< curPop[i][j] + cargas[j][1]:
-                curAptidao[i] = curAptidao[i] + cargas[j][0]*custoAtual 
+        for i in range(0,len(curPop)):       
+            for j in range(0, len(cargas)):
+                if t >= curPop[i][j] and t< curPop[i][j] + cargas[j][1]:
+                    curAptidao[i] = curAptidao[i] + cargas[j][0]*custoAtual 
                 
     return curAptidao
 
+# Seleção
+
+def selec(pop, custos, cargas):
+    interPop = np.zeros(len(pop))
+    curAptidao = f(cargas, custos, pop)
+    bestGen = [0,curAptidao[0]]
+    
+    for i in range(0,len(pop)):
+        if bestGen[1]>curAptidao[i]:
+            bestGen[1] = curAptidao[i]
+            bestGen[0] = i
+    
+    interPop[0] = pop[bestGen[0]]
+    
+    for i in range(1,len(pop)):
+        m = np.random.randint(0,high=len(pop))
+        n = np.random.randint(0,high=len(pop))
+        
+        if curAptidao[m] > curAptidao[n] :
+            interPop[i] = pop[n]
+        else:
+            interPop[i] = pop[m]
+    
+    return interPop
+
+# Crossover
+
+#def cross(pop, rtCross):
+
+        
 # Parâmetros do Algoritmo genético
 
 sizePop = 100
@@ -31,6 +64,7 @@ maxDem = 200 #Valor Máximo de demanda energética
 limFunc = [0,maxDem] 
 custos = [[0.5, 0, 5],[1, 6, 16], [2, 16, 23]] #[preço, começo, termino]
 cargas = [[8,3],[7,2],[10,1],[0.5,12]]#[consumo, tempo]
+
 
 # Criando População inicial
 
